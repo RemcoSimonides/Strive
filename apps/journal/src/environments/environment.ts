fb31e194e5c4ab6307484c6debd402b12176b143
Remco <RemcoSimonides@gmail.com>
2020-08-17 08:58:38 +0200

    Added environment variables

--- apps/journal/src/environments/environment.ts
+++ apps/journal/src/environments/environment.ts
@@ -1,16 +1,28 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyD2kHICqwdpVYlaDrGESHS3O8Qss0mLdsk",
+    authDomain: "lets-support-23a6c.firebaseapp.com",
+    databaseURL: "https://lets-support-23a6c.firebaseio.com",
+    projectId: "lets-support-23a6c",
+    storageBucket: "lets-support-23a6c.appspot.com",
+    messagingSenderId: "156079640882",
+    appId: "1:156079640882:web:93e99452da064966",
+    measurementId: "G-3J20RJ6EVP"
+  },
+  algolia: {
+    appId: "OGEMTU2VFW",
+    apiKey: "db3455ecf457168f65d91c4238522f04"
+  }
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
