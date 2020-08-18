021bc863deb71330ed630b2089cc3f3162667852
Remco <RemcoSimonides@gmail.com>
2020-08-18 22:10:20 +0200

    Added root files

--- apps/journal/src/app/app.component.ts
+++ apps/journal/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'strive-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
