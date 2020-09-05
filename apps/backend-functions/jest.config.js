aa54129d0666feb1cb04ec0f31a34005a46d9a88
Remco <RemcoSimonides@gmail.com>
2020-09-05 17:45:13 +0200

    Added backend functions 🎉

---
+++ apps/backend-functions/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'backend-functions',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  coverageDirectory: '../../coverage/apps/backend-functions'
+};
