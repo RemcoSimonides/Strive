091f2c82653b5885ad593d7f45fb9fe1d8d4813d
RemcoSimonides <remcosimonides@gmail.com>
2021-09-19 09:59:31 +0200

    Add home to navbar desktop and move notifications

--- apps/journal/src/app/pages/profile/popovers/profile-options-browser/profile-options-browser.page.ts
+++ apps/journal/src/app/pages/profile/popovers/profile-options-browser/profile-options-browser.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { PopoverController, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
-// Services
 import { Auth } from '@angular/fire/auth';
+import { limit, where } from '@angular/fire/firestore';
+import { PopoverController, ModalController } from '@ionic/angular';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+// Services
 import { UserService } from '@strive/user/user/+state/user.service';
 // Components
 import { AuthModalPage, enumAuthSegment } from '@strive/user/auth/components/auth-modal/auth-modal.page';
 import { FcmService } from '@strive/utils/services/fcm.service';
+import { NotificationService } from '@strive/notification/+state/notification.service';
 
 @Component({
   selector: 'app-profile-options-browser',
@@ -15,17 +19,32 @@ import { FcmService } from '@strive/utils/services/fcm.service';
 })
 export class ProfileOptionsBrowserPage {
 
+  unreadNotifications$: Observable<boolean>
+
   constructor(
     private auth: Auth,
     private modalCtrl: ModalController,
     private popoverCtrl: PopoverController,
     private router: Router,
     private user: UserService,
-    private fcm: FcmService
-  ) { }
+    private fcm: FcmService,
+    private notification: NotificationService
+  ) {
+    this.unreadNotifications$ = this.user.profile$.pipe(
+      switchMap(profile => profile
+        ? this.notification.valueChanges([where('type', '==', 'notification'), where('isRead', '==', false), limit(1)], { uid: profile.uid }).pipe(map(notifications => !!notifications.length))
+        : of(false)
+      )
+    )
+  }
 
   goToProfile() {
     this.router.navigateByUrl(`/profile/${this.user.uid}`)
+    this.popoverCtrl.dismiss()
+  }
+
+  goToNotifications() {
+    this.router.navigateByUrl(`/notifications`)
     this.popoverCtrl.dismiss()
   }
 
