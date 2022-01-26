3e03e8aace35d7a76c60f68cd129a84842f3734c
Remco <RemcoSimonides@gmail.com>
2022-01-26 19:44:07 +0100

    placeholder

--- libs/goal/src/lib/goal/components/upsert/slides/slide-2/slide-2.component.ts
+++ libs/goal/src/lib/goal/components/upsert/slides/slide-2/slide-2.component.ts
@@ -16,10 +16,11 @@ export class Slide2Component implements OnDestroy {
 
   @Output() stepper = new EventEmitter<'next' | 'previous'>()
 
-  fields: Record<string, { title: string, description: string }> = {
+  fields: Record<string, { title: string, description: string, placeholder: string }> = {
     why: {
       title: 'Why do you want to achieve this goal?',
-      description: `Connect your purpose with this goal. Why do you want to achieve this goal?`
+      description: `Connect your purpose with this goal. Why do you want to achieve this goal?`,
+      placeholder: `e.g. working on this goal will improve my lifestyle and I will feel more energetic, healthy and ...`
     },
     // where: {
     //   title: 'Where will it take place?',
@@ -27,15 +28,18 @@ export class Slide2Component implements OnDestroy {
     // },
     inspiration: {
       title: 'What or who inspired you?',
-      description: 'Being reminded of what or who inspired you to start this goal will motivate you to keep striving during hard moments.'
+      description: 'Being reminded of what or who inspired you to start this goal will motivate you to keep striving during hard moments.',
+      placeholder: `e.g. listened to an interesting podcast about how to life longer ...`
     },
     stop: {
       title: 'What is holding you back?',
       description: `What is stopping you? Are you stuck in your bad habits? Not having enough time? Why don't you do less of what you rather not do.`,
+      placeholder: `e.g. afraid of my friends and family raising their eyebrows when they notice ...`
     },
     else: {
       title: 'Anything else',
-      description: `Anything else you'd like to mention. Where will it take place? A special thanks to family or friends? anything`
+      description: `Anything else you'd like to mention. Where will it take place? A special thanks to family or friends? anything`,
+      placeholder: `e.g. location, shout outs, sources of inspiration, ...`
     }
   }
 
