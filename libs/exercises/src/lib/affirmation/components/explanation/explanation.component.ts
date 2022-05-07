1428a5d35c17bb531a7fdab30d93663d1eb29a77
Remco <RemcoSimonides@gmail.com>
2022-05-07 18:10:35 +0200

    close popover on back

--- libs/exercises/src/lib/affirmation/components/explanation/explanation.component.ts
+++ libs/exercises/src/lib/affirmation/components/explanation/explanation.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, HostBinding, HostListener } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'exercise-affirmation-explanation',
   templateUrl: 'explanation.component.html',
   styleUrls: ['./explanation.component.scss']
 })
-export class AffirmationExplanationComponent {}
+export class AffirmationExplanationComponent {
+  @HostListener('window:popstate', ['$event'])
+  onPopState() {
+    this.popoverCtrl.dismiss()
+  }
+  @HostBinding() popover: HTMLIonPopoverElement
+
+  constructor(
+    private location: Location,
+    private popoverCtrl: PopoverController
+  ) {
+    window.history.pushState(null, null, window.location.href)
+  }
+
+  async ngOnInit() {
+    this.popover.onWillDismiss().then(res => {
+      if (res.role === 'backdrop') this.location.back()
+    })
+  }
+}
