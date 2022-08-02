e82969bb9cf8ae68acd114064e9356f2356416d8
Remco <RemcoSimonides@gmail.com>
2022-08-02 15:46:54 +0200

    fix auth modal

--- apps/journal/src/app/pages/home/home.page.ts
+++ apps/journal/src/app/pages/home/home.page.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { GoalService } from '@strive/goal/goal/goal.service';
 import { exercises } from '@strive/exercises/utils';
 import { PWAService } from '@strive/utils/services/pwa.service';
+import { AuthModalComponent, enumAuthSegment } from "@strive/user/auth/components/auth-modal/auth-modal.page";
+import { ModalController } from "@ionic/angular";
 
 @Component({
   selector: 'journal-home',
@@ -12,11 +14,22 @@ import { PWAService } from '@strive/utils/services/pwa.service';
 })
 export class HomeComponent {
 
-  goals$ = this.goal.valueChanges(['kWqyr9RQeroZ1QjsSmfU', 'pGvDUf2aWP7gt5EnIEjt', 'UU9oRpCmKIljnTy4JFlL', 'NJQ4AwTN7y0o7Dx0NoNB'])
+  goals$ = this.goal.valueChanges(['CWIYC1SOxfZYdriIIRZZ', 'pGvDUf2aWP7gt5EnIEjt', '6wxXjXMNcPYDhUKqQPDJ', 'XWi3By2rY2E05M4Snjtx'])
   exercises = exercises
+  enumAuthSegment = enumAuthSegment
 
   constructor (
     private goal: GoalService,
+    private modalCtrl: ModalController,
     public pwa: PWAService
   ) {}
+
+  openAuthModal() {
+    this.modalCtrl.create({
+      component: AuthModalComponent,
+      componentProps: {
+        authSegment: enumAuthSegment.register
+      }
+    }).then(modal => modal.present())
+  }
 }
