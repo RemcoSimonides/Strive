daa045a1fd4ef933cbf7ca70d562cdd599bdddee
Remco <RemcoSimonides@gmail.com>
2022-08-11 15:29:26 +0200

    Remove from goal

--- libs/goal/src/lib/stakeholder/popovers/roles/roles.component.ts
+++ libs/goal/src/lib/stakeholder/popovers/roles/roles.component.ts
@@ -1,7 +1,7 @@
 import { Location } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { PopoverController } from '@ionic/angular';
+import { AlertController, PopoverController } from '@ionic/angular';
 import { GoalStakeholder } from '@strive/model';
 import { delay } from '@strive/utils/helpers';
 import { GoalStakeholderService } from '../../stakeholder.service';
@@ -17,6 +17,7 @@ export class RolesPopoverComponment {
 	@Input() stakeholder!: GoalStakeholder
 
 	constructor(
+    private alertCtrl: AlertController,
     private location: Location,
 		private popoverCtrl: PopoverController,
     private router: Router,
@@ -38,5 +39,27 @@ export class RolesPopoverComponment {
     delay(250).then(_ => {
       this.router.navigate(['/profile/', this.stakeholder.uid])
     })
+  }
+
+  async remove() {
+    this.popoverCtrl.dismiss()
+
+    const { isSupporter } = this.stakeholder
+    return this.alertCtrl.create({
+      subHeader: `Are you sure you want to remove ${this.stakeholder.username} from this goal?`,
+      message: isSupporter ? `${this.stakeholder.username}'s supports will be canceled` : '',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: async () => {
+            this.stakeholderService.remove(this.stakeholder.uid, { params: { goalId: this.goalId } })
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
+    }).then(alert => alert.present())
   }
 }
