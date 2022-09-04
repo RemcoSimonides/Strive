617dbb14bd5004891c00af9a1140e67dc0dfc1f9
Remco <RemcoSimonides@gmail.com>
2022-09-04 18:01:31 +0200

    add border radius

--- apps/backend-functions/src/migration.ts
+++ apps/backend-functions/src/migration.ts
@@ -13,39 +13,39 @@ export const migrate = functions.https.onRequest(async (req, res) => {
   try {
 
     // get all goal events
-    const goalEventsSnap = await db.collection('GoalEvents').get()
-    const batch = db.batch()
+    // const goalEventsSnap = await db.collection('GoalEvents').get()
+    // const batch = db.batch()
 
-    for (const doc of goalEventsSnap.docs) {
-      const event = createGoalEvent(toDate(doc.data()))
+    // for (const doc of goalEventsSnap.docs) {
+    //   const event = createGoalEvent(toDate(doc.data()))
 
-      const name = renameEvent(event.name)
-      if (name) {
-        batch.update(doc.ref, { name })
-      }
-    }
-    await batch.commit()
+    //   const name = renameEvent(event.name)
+    //   if (name) {
+    //     batch.update(doc.ref, { name })
+    //   }
+    // }
+    // await batch.commit()
 
-    // get all story events
-    const goalsSnap = await db.collection('Goals').get()
-    const storyBatch = db.batch()
+    // // get all story events
+    // const goalsSnap = await db.collection('Goals').get()
+    // const storyBatch = db.batch()
 
-    for (const { id, ref } of goalsSnap.docs) {
-      const storySnap = await db.collection(`Goals/${id}/Story`).get()
+    // for (const { id, ref } of goalsSnap.docs) {
+    //   const storySnap = await db.collection(`Goals/${id}/Story`).get()
 
-      for (const doc of storySnap.docs) {
-        const storyItem = createStoryItem(doc.data())
+    //   for (const doc of storySnap.docs) {
+    //     const storyItem = createStoryItem(doc.data())
 
-        const name = renameEvent(storyItem.name)
-        if (name) {
-          logger.log('story item: ', storyItem)
-          logger.log('ref: ', doc.ref.path)
-          storyBatch.update(doc.ref, { name })
-        }
-      }
-    }
+    //     const name = renameEvent(storyItem.name)
+    //     if (name) {
+    //       logger.log('story item: ', storyItem)
+    //       logger.log('ref: ', doc.ref.path)
+    //       storyBatch.update(doc.ref, { name })
+    //     }
+    //   }
+    // }
 
-    await storyBatch.commit()
+    // await storyBatch.commit()
 
     res.status(200).send('all good')
   } catch (err) {
