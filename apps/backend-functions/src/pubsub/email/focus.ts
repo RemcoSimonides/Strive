12103166f2b9c865e50080d3ebbdd99de7e113df
Remco <RemcoSimonides@gmail.com>
2022-09-16 09:38:45 +0200

    dont send weekly focus if goal is newer than 4 days

--- apps/backend-functions/src/pubsub/email/focus.ts
+++ apps/backend-functions/src/pubsub/email/focus.ts
@@ -7,6 +7,7 @@ import { groupIds, templateIds } from './ids'
 import { sendMailFromTemplate } from '../../shared/sendgrid/sendgrid'
 import { toDate } from '../../shared/utils'
 import { getImgIxResourceUrl } from '../../shared/image/image'
+import { isAfter, subDays } from 'date-fns'
 
 
 function getFocusData(goal: Goal, stakeholder: GoalStakeholder): { goal: GoalLink, stakeholder: GoalStakeholder } {
@@ -43,6 +44,8 @@ export const scheduledFocusEmailRunner = functions.pubsub.schedule('0 18 * * 5')
     const goal = goals.find(goal => goal.id === stakeholder.goalId)
     const personal = personals.find(personal => personal.uid === stakeholder.uid)
 
+    if (newerThanFourDays(goal)) continue
+
     const data = getFocusData(goal, stakeholder)
 
     await sendMailFromTemplate({
@@ -60,3 +63,8 @@ export const scheduledFocusEmailRunner = functions.pubsub.schedule('0 18 * * 5')
     }, groupIds.unsubscribeAll)
   }
 })
+
+function newerThanFourDays(goal: Goal): boolean {
+  const fourDaysAgo = subDays(new Date(), 4)
+  return isAfter(goal.createdAt, fourDaysAgo)
+}
