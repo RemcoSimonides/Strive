1648cdc886d0ae7b706a4ed239ae4ab77e58b44f
Remco <RemcoSimonides@gmail.com>
2022-09-26 15:38:40 +0200

    remove flexlayout

--- apps/journal/src/app/pages/exercises/dear-future-self/dear-future-self.module.ts
+++ apps/journal/src/app/pages/exercises/dear-future-self/dear-future-self.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
+import { IonicModule } from '@ionic/angular'
 
-import { DearFutureSelfComponent } from './dear-future-self.component';
-import { DearFutureSelfExplanationComponent } from '@strive/exercises/dear-future-self/components/explanation/explanation.component';
-import { MessagePopoverComponent } from '@strive/exercises/dear-future-self/components/message/message.component';
-import { HeaderModule } from '@strive/ui/header/header.module';
+import { DearFutureSelfComponent } from './dear-future-self.component'
+import { DearFutureSelfExplanationComponent } from '@strive/exercises/dear-future-self/components/explanation/explanation.component'
+import { MessagePopoverComponent } from '@strive/exercises/dear-future-self/components/message/message.component'
+import { HeaderModule } from '@strive/ui/header/header.module'
 
-import { PageLoadingModule } from '@strive/ui/page-loading/page-loading.module';
-import { AuthModalModule } from '@strive/user/auth/components/auth-modal/auth-modal.module';
+import { PageLoadingModule } from '@strive/ui/page-loading/page-loading.module'
+import { AuthModalModule } from '@strive/user/auth/components/auth-modal/auth-modal.module'
 
-import { TimeToGoPipeModule } from '@strive/utils/pipes/time-to-go.pipe';
-import { TimeAgoPipeModule } from '@strive/utils/pipes/time-ago.pipe';
+import { TimeToGoPipeModule } from '@strive/utils/pipes/time-to-go.pipe'
+import { TimeAgoPipeModule } from '@strive/utils/pipes/time-ago.pipe'
 
 const routes: Routes = [
   {
@@ -27,7 +26,6 @@ const routes: Routes = [
   imports: [
     CommonModule,
     IonicModule,
-    FlexLayoutModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
     TimeToGoPipeModule,
