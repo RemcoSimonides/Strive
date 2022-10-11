a6278f5c63a6b07c384e72fc099531bb8c601c37
Remco <RemcoSimonides@gmail.com>
2022-10-11 10:48:11 +0200

    decrypt affirmation on backend side only

--- libs/exercises/src/lib/affirmation/modals/affirm-modal.component.ts
+++ libs/exercises/src/lib/affirmation/modals/affirm-modal.component.ts
@@ -1,12 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
-import { Location } from '@angular/common'
-import { ModalController } from '@ionic/angular'
-
-import { BehaviorSubject, filter, switchMap } from 'rxjs'
-import { AES, enc } from 'crypto-js'
-
 import { ModalDirective } from '@strive/utils/directives/modal.directive'
-import { PersonalService } from '@strive/user/personal/personal.service'
 
 @Component({
   selector: '[affirmation] exercise-affirm-modal',
@@ -15,26 +8,5 @@ import { PersonalService } from '@strive/user/personal/personal.service'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AffirmModalComponent extends ModalDirective {
-
-  _affirmation$ = new BehaviorSubject<string>('')
-  affirmation$ = this._affirmation$.asObservable().pipe(
-    filter(affirmation => !!affirmation),
-    switchMap(async affirmation => {
-      const key = await this.personalService.getEncryptionKey()
-      return AES.decrypt(affirmation, key).toString(enc.Utf8)
-    })
-  )
-
-  @Input() set affirmation(affirmation: string) {
-    this._affirmation$.next(affirmation)
-  }
-
-  constructor(
-    location: Location,
-    modalCtrl: ModalController,
-    private personalService: PersonalService
-  ) {
-    super(location, modalCtrl)
-  }
-
+  @Input() affirmation!: string
 }
