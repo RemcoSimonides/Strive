01fc1895005c34074789ad55a6ab7c28618454fb
Remco <RemcoSimonides@gmail.com>
2022-10-11 10:03:02 +0200

    fix affirmation encryption

--- libs/exercises/src/lib/affirmation/affirmation.service.ts
+++ libs/exercises/src/lib/affirmation/affirmation.service.ts
@@ -3,23 +3,14 @@ import { DocumentSnapshot, serverTimestamp } from 'firebase/firestore'
 import { toDate, FireSubCollection } from 'ngfire'
 
 import { Affirmations } from '@strive/model'
-import { PersonalService } from '@strive/user/personal/personal.service'
-import { AES } from 'crypto-js'
 
 @Injectable({providedIn: 'root'})
 export class AffirmationService extends FireSubCollection<Affirmations> {
   readonly path = 'Users/:uid/Exercises'
   override readonly memorize = true
 
-  constructor(private personalService: PersonalService) {
-    super()
-  }
-
   protected override async toFirestore(settings: Affirmations, actionType: 'add' | 'update'): Promise<Affirmations> {
     const timestamp = serverTimestamp() as any
-
-    const key = await this.personalService.getEncryptionKey()
-    settings.affirmations = settings.affirmations.map(affirmation => AES.encrypt(affirmation, key).toString())
 
     if (actionType === 'add') settings.createdAt = timestamp
     settings.updatedAt = timestamp
