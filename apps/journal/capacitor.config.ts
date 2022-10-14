2651f80d5fd07d98bfd43eacc44aa11ff60febe0
Remco <RemcoSimonides@gmail.com>
2022-10-14 09:16:54 +0200

    add support for capacitor cli

---
+++ apps/journal/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.strive.journal',
+  appName: 'journal',
+  webDir: '../../dist/apps/journal/browser',
+  bundledWebRuntime: false,
+};
+
+export default config;
