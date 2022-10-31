de284f63d3ebbc2ded598688860f58d311e2f4ec
Remco <RemcoSimonides@gmail.com>
2022-10-31 11:10:16 +0100

    update privacy policy and ui

--- libs/ui/src/lib/static-information/privacy/privacy-policy.module.ts
+++ libs/ui/src/lib/static-information/privacy/privacy-policy.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PrivacyPolicyComponent } from './privacy-policy.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { PrivacyPolicyComponent } from './privacy-policy.component'
 
 @NgModule({
   imports: [CommonModule],
--- libs/ui/src/lib/static-information/terms/terms.component.ts
+++ libs/ui/src/lib/static-information/terms/terms.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'strive-terms',
-  templateUrl: './terms.component.html'
+  styleUrls: ['./terms.component.scss'],
+  templateUrl: './terms.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TermsComponent { }
