d125b44d13e91bd0d7f9198923b6a22adcc411fd
Remco <RemcoSimonides@gmail.com>
2022-11-08 15:41:46 +0100

    resize view when keyboard

--- apps/journal/capacitor.config.ts
+++ apps/journal/capacitor.config.ts
@@ -1,4 +1,5 @@
 import { CapacitorConfig } from '@capacitor/cli'
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard'
 
 const config: CapacitorConfig = {
   appId: 'com.strive.journal',
@@ -6,6 +7,10 @@ const config: CapacitorConfig = {
   webDir: '../../dist/apps/journal/browser',
   bundledWebRuntime: false,
   plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Ionic,
+      style: KeyboardStyle.Dark
+    },
     GoogleAuth: {
       scopes: ['profile', 'email'],
       serverClientId: '423468347975-tjkdd38gna8rfgqd16f0jpf1o5bl6204.apps.googleusercontent.com',
