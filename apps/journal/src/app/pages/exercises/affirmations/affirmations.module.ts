7520e10e56f64e0758094d11268eaed94831adad
Remco <RemcoSimonides@gmail.com>
2022-12-17 13:06:21 +0100

    add header to chat

--- apps/journal/src/app/pages/exercises/affirmations/affirmations.module.ts
+++ apps/journal/src/app/pages/exercises/affirmations/affirmations.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule, Routes } from '@angular/router'
+import { ReactiveFormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
 
-import { AffirmationsComponent } from './affirmations.component';
+import { AffirmationsComponent } from './affirmations.component'
 
-import { PageLoadingModule } from '@strive/ui/page-loading/page-loading.module';
-import { ActivatePushNotificaitonsModule } from '@strive/exercises/components/activate-push-notifications/activate-push-notifications.module';
-import { HeaderModule } from '@strive/ui/header/header.module';
-import { DatetimeModule } from '@strive/ui/datetime/datetime.module';
+import { PageLoadingModule } from '@strive/ui/page-loading/page-loading.module'
+import { ActivatePushNotificaitonsModule } from '@strive/exercises/components/activate-push-notifications/activate-push-notifications.module'
+import { HeaderModule } from '@strive/ui/header/header.module'
+import { DatetimeModule } from '@strive/ui/datetime/datetime.module'
 
 const routes: Routes = [
   {
