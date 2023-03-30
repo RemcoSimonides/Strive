9882307010944269705ff709282534208aee3a96
Bamco <remcosimonides@gmail.com>
2023-03-30 22:14:07 +0200

    show empty state in supporters modal

--- libs/stakeholder/src/lib/modals/supporters/supporters.component.ts
+++ libs/stakeholder/src/lib/modals/supporters/supporters.component.ts
@@ -7,10 +7,13 @@ import { combineLatest, map, Observable } from 'rxjs'
 
 import { ImageModule } from '@strive/media/directives/image.module'
 import { RolesPopoverComponent } from '@strive/stakeholder/popovers/roles/roles.component'
+import { AuthModalComponent, enumAuthSegment } from '@strive/auth/components/auth-modal/auth-modal.page'
+import { AddSupportModalComponent } from '@strive/support/modals/add/add.component'
 
 import { AuthService } from '@strive/auth/auth.service'
 import { GoalStakeholderService } from '@strive/stakeholder/stakeholder.service'
 import { ProfileService } from '@strive/user/profile.service'
+import { GoalService } from '@strive/goal/goal.service'
 
 import { ModalDirective } from '@strive/utils/directives/modal.directive'
 import { createGoalStakeholder, GoalStakeholder, Stakeholder } from '@strive/model'
@@ -25,6 +28,7 @@ import { createGoalStakeholder, GoalStakeholder, Stakeholder } from '@strive/mod
   ],
   selector: '[goalId] strive-supporters-modal',
   templateUrl: './supporters.component.html',
+  styleUrls: ['./supporters.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SupportersModalComponent extends ModalDirective implements OnInit {
@@ -38,6 +42,7 @@ export class SupportersModalComponent extends ModalDirective implements OnInit {
 
   constructor(
     private auth: AuthService,
+    private goalService: GoalService,
     protected override location: Location,
     protected override modalCtrl: ModalController,
     private popoverCtrl: PopoverController,
@@ -60,7 +65,7 @@ export class SupportersModalComponent extends ModalDirective implements OnInit {
       map(([ user, stakeholders ]) => {
         const you = stakeholders.find(stakeholder => stakeholder.uid === user?.uid)
         const others = stakeholders.filter(stakeholder => stakeholder.isSupporter).filter(stakeholder => stakeholder.uid !== user?.uid)
-        return { 
+        return {
           you: you ? you : createGoalStakeholder(),
           others
         }
@@ -82,5 +87,26 @@ export class SupportersModalComponent extends ModalDirective implements OnInit {
       },
       event
     }).then(popover => popover.present())
+  }
+
+  async support() {
+    if (!this.auth.uid) {
+      const modal = await this.modalCtrl.create({
+        component: AuthModalComponent,
+        componentProps: {
+          authSegment: enumAuthSegment.login
+        }
+      })
+      modal.onDidDismiss().then(({ data: loggedIn }) => {
+        if (loggedIn) this.support()
+      })
+      return modal.present()
+    }
+
+    const goal = await this.goalService.getValue(this.goalId)
+    this.modalCtrl.create({
+      component: AddSupportModalComponent,
+      componentProps: { goal }
+    }).then(modal => modal.present())
   }
 }
