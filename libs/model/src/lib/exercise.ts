3939b9bdb9530870c151ef5b032e36770d294152
Bamco <remcosimonides@gmail.com>
2023-04-15 11:07:03 +0200

    update explore and exercise images

--- libs/model/src/lib/exercise.ts
+++ libs/model/src/lib/exercise.ts
@@ -6,34 +6,34 @@ export interface Exercise {
 	image: string
 	link: string
 	description: string
-} 
+}
 
 export const exercises: Exercise[] = [
 	{
 		id: 'Affirmations',
 		title: 'Affirmations',
-		image: 'exercise_affirmations.jpeg',
+		image: 'exercise_affirmations.png',
 		link: '/exercise/affirmations',
 		description: 'Control your thoughts by regularly repeating short, powerful statements'
 	},
 	{
 		id: 'DailyGratitude',
 		title: 'Daily Gratitude',
-		image: 'exercise_daily_gratitude.jpeg',
+		image: 'exercise_daily_gratitude.png',
 		link: '/exercise/daily-gratitude',
 		description: 'A daily practice that helps you to focus on the positive over the negative'
 	},
 	{
 		id: 'DearFutureSelf',
 		title: 'Dear Future Self',
-		image: 'exercise_dear_future_self.jpeg',
+		image: 'exercise_dear_future_self.png',
 		link: '/exercise/dear-future-self',
 		description: 'Write a message to yourself in the (far) future'
 	},
 	{
 		id: 'WheelOfLife',
 		title: 'Wheel of Life',
-		image: 'exercise_wheel_of_life.jpg',
+		image: 'exercise_wheel_of_life.png',
 		link: '/exercise/wheel-of-life',
 		description: 'Get insight in which areas in your life needs more attention'
 	}
