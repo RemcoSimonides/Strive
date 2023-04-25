e9c3825dd3c29189c259beaceb1db76e25e72e21
Bamco <remcosimonides@gmail.com>
2023-04-25 20:39:52 +0200

    dont show deleted milestones

--- apps/journal/src/app/pages/goal/goal.page.ts
+++ apps/journal/src/app/pages/goal/goal.page.ts
@@ -217,7 +217,7 @@ export class GoalPageComponent implements OnDestroy {
     this.supports$ = supports$.pipe(map(groupByObjective), map(sortGroupedSupports))
 
     this.milestones$ = combineLatest([goalId$, this.roadmapOrder$]).pipe(
-      switchMap(([goalId, order]) => goalId ? this.milestoneService.valueChanges([orderBy('order', order)], { goalId }) : of([])),
+      switchMap(([goalId, order]) => goalId ? this.milestoneService.valueChanges([where('deletedAt', '==', null), orderBy('order', order)], { goalId }) : of([])),
       joinWith({
         achiever: ({ achieverId }) => achieverId ? this.profileService.valueChanges(achieverId) : undefined,
         supports: milestone => supports$.pipe(
