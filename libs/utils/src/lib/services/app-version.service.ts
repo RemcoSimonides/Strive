4e2e49831947489ee4d277568b11ff1c4160a5aa
Bamco <remcosimonides@gmail.com>
2023-05-16 20:21:01 +0200

    update version

--- libs/utils/src/lib/services/app-version.service.ts
+++ libs/utils/src/lib/services/app-version.service.ts
@@ -14,7 +14,7 @@ interface Version {
 export class AppVersionService extends FireDocument<Version> {
   override path = `meta/version`
 
-  version = "1.8.7"
+  version = "1.8.8"
 
   constructor(
     private alertCtrl: AlertController,
