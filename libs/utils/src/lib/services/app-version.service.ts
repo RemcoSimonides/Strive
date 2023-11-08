1436ec0791b88ded31f13400304f114bb33eda01
Bamco <remcosimonides@gmail.com>
2023-11-08 21:26:08 +0100

    update version and node module file path

--- libs/utils/src/lib/services/app-version.service.ts
+++ libs/utils/src/lib/services/app-version.service.ts
@@ -14,7 +14,7 @@ interface Version {
 export class AppVersionService extends FireDocument<Version> {
   override path = `meta/version`
 
-  version = "1.9.2"
+  version = "1.9.3"
 
   constructor(
     private alertCtrl: AlertController,
