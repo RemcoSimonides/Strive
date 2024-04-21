b392c61c59aa1cae7730127979e41c0001da8b7c
Bamco <remcosimonides@gmail.com>
2024-04-21 11:20:45 +0200

    remove unused component

--- apps/journal/src/app/pages/explore/explore.module.ts
+++ apps/journal/src/app/pages/explore/explore.module.ts
@@ -7,7 +7,6 @@ import { ExplorePageComponent } from './explore.page'
 // Strive
 import { ThumbnailListModule } from '@strive/ui/thumbnail/layout/list/thumbnail-list.module'
 import { SmallThumbnailModule } from '@strive/ui/thumbnail/components/small/small-thumbnail.module'
-import { LargeThumbnailModule } from '@strive/ui/thumbnail/components/large/large-thumbnail.module'
 import { RowsPipeModule } from '@strive/ui/thumbnail/pipes/rows.pipe'
 import { ImageModule } from '@strive/media/directives/image.module'
 import { HeaderComponent } from '@strive/ui/header/header.component'
@@ -31,7 +30,6 @@ const routes: Routes = [
     // Strive
     ThumbnailListModule,
     SmallThumbnailModule,
-    LargeThumbnailModule,
     RowsPipeModule,
     ImageModule,
     HeaderComponent,
