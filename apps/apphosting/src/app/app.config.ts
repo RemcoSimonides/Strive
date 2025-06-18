cd4f2f4b3a6215bfbcb393037f5304e467bc1f5e
Bamco <remcosimonides@gmail.com>
2025-06-18 22:05:43 +0200

    add ionic to apphosting

--- apps/apphosting/src/app/app.config.ts
+++ apps/apphosting/src/app/app.config.ts
@@ -5,11 +5,18 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideIonicAngular({ mode: 'md' }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
 };
