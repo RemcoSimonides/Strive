71addafaea69277fec68d1efe50156d83ebcf87c
Bamco <remcosimonides@gmail.com>
2025-06-29 09:38:26 +0200

    project json

--- apps/randomname/src/main.ts
+++ apps/randomname/src/main.ts
@@ -1,7 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+import { AppComponent } from './app/app.component'
+import { appConfig } from './app/app.config'
+import { environment } from '@env'
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
+import '@angular/localize/init';
+
+defineCustomElements(window)
+
+if (environment.production) {
+  enableProdMode()
+}
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err))
