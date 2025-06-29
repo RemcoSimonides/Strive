c292a6aa38a5679c7166cb31d5d6ce1a15d6d60e
Bamco <remcosimonides@gmail.com>
2025-06-29 09:21:43 +0200

    1

--- apps/randomname/eslint.config.cjs
+++ apps/randomname/eslint.config.cjs
@@ -12,7 +12,7 @@ module.exports = [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'journal',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ module.exports = [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'journal',
           style: 'kebab-case',
         },
       ],
--- apps/randomname/src/app/app.component.ts
+++ apps/randomname/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule],
+  selector: 'journal-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
