70eaf70e3e574e3c2d8e232e94afb3275928aa63
Bamco <remcosimonides@gmail.com>
2025-06-29 09:55:26 +0200

    app config except ngfire

--- apps/randomname/src/app/app.config.ts
+++ apps/randomname/src/app/app.config.ts
@@ -1,15 +1,42 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker'
+
+import { provideIonicAngular } from '@ionic/angular/standalone'
+
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+
+// Sentry
+import { init, createErrorHandler } from '@sentry/angular'
+
+init({
+  dsn: 'https://4f1406746eae4c7aa069055270c617d9@o1354459.ingest.sentry.io/6638131',
+  release: 'strivejournal@1',
+  dist: '1',
+  tracesSampleRate: 1.0,
+})
+
+// Swiper
+import { register } from 'swiper/element/bundle'
+register()
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideIonicAngular({ mode: 'md' }),
+    provideServiceWorker('sw-master.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    { provide: 'APP_NAME', useValue: 'journal' },
+    { provide: ErrorHandler, useValue: createErrorHandler() },
   ],
 };
