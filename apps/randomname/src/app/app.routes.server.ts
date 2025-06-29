8c50e8c890bde05482ca0dc24d4d029b18be179c
Bamco <remcosimonides@gmail.com>
2025-06-29 20:19:55 +0200

    Server side rendering for id pages

--- apps/randomname/src/app/app.routes.server.ts
+++ apps/randomname/src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'goal/:id',
+    renderMode: RenderMode.Ssr,
+  },
+  {
+    path: 'profile/:id',
+    renderMode: RenderMode.Ssr,
+  },
+  {
+    path: 'supports/:id',
+    renderMode: RenderMode.Ssr,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
