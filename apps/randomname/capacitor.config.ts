851649515b9d818cf729c15d567db7c34c1b4ee7
Bamco <remcosimonides@gmail.com>
2025-06-29 09:45:11 +0200

    capacitor config

---
+++ apps/randomname/capacitor.config.ts
@@ -0,0 +1,26 @@
+import { CapacitorConfig } from '@capacitor/cli'
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard'
+
+const config: CapacitorConfig = {
+  appId: 'com.strive.journal',
+  appName: 'Strive Journal',
+  webDir: '../../dist/apps/journal/browser',
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Ionic,
+      style: KeyboardStyle.Dark
+    },
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['apple.com', 'google.com']
+    },
+    SplashScreen: {
+      launchAutoHide: false
+    }
+  },
+  server: {
+    androidScheme: "http" // https://capacitorjs.com/docs/updating/5-0#update-androidscheme
+  }
+}
+
+export default config
